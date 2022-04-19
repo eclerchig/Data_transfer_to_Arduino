c40ec59853cad17d6c4a8a5a364448af2a7f6777
Eclerchig <valeria.eclerchig@yandex.ru>
2022-04-19 20:20:05 +0800

    init

---
+++ WindowsSerial/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace WindowsSerial
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.comPortscomboBox = new System.Windows.Forms.ComboBox();
+			this.connectButton = new System.Windows.Forms.Button();
+			this.labelTemp = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.UpdatePortsButton = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.AnswerTextBox = new System.Windows.Forms.TextBox();
+			this.CommandTextBox = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SendCommandButton = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// comPortscomboBox
+			// 
+			this.comPortscomboBox.FormattingEnabled = true;
+			this.comPortscomboBox.Location = new System.Drawing.Point(57, 53);
+			this.comPortscomboBox.Name = "comPortscomboBox";
+			this.comPortscomboBox.Size = new System.Drawing.Size(121, 24);
+			this.comPortscomboBox.TabIndex = 0;
+			// 
+			// connectButton
+			// 
+			this.connectButton.Location = new System.Drawing.Point(57, 126);
+			this.connectButton.Name = "connectButton";
+			this.connectButton.Size = new System.Drawing.Size(121, 24);
+			this.connectButton.TabIndex = 1;
+			this.connectButton.Text = "Connect";
+			this.connectButton.UseVisualStyleBackColor = true;
+			this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+			// 
+			// labelTemp
+			// 
+			this.labelTemp.AutoSize = true;
+			this.labelTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.labelTemp.Location = new System.Drawing.Point(257, 219);
+			this.labelTemp.Name = "labelTemp";
+			this.labelTemp.Size = new System.Drawing.Size(27, 29);
+			this.labelTemp.TabIndex = 2;
+			this.labelTemp.Text = "0";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(33, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(172, 17);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Выбор доступного порта";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+			this.groupBox1.Controls.Add(this.UpdatePortsButton);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.comPortscomboBox);
+			this.groupBox1.Controls.Add(this.connectButton);
+			this.groupBox1.Location = new System.Drawing.Point(24, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(241, 162);
+			this.groupBox1.TabIndex = 4;
+			this.groupBox1.TabStop = false;
+			// 
+			// UpdatePortsButton
+			// 
+			this.UpdatePortsButton.Location = new System.Drawing.Point(57, 95);
+			this.UpdatePortsButton.Name = "UpdatePortsButton";
+			this.UpdatePortsButton.Size = new System.Drawing.Size(121, 23);
+			this.UpdatePortsButton.TabIndex = 4;
+			this.UpdatePortsButton.Text = "Get COM-ports";
+			this.UpdatePortsButton.UseVisualStyleBackColor = true;
+			this.UpdatePortsButton.Click += new System.EventHandler(this.UpdatePortsButton_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(202, 188);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(162, 17);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Величина напряжения:";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.AnswerTextBox);
+			this.groupBox2.Controls.Add(this.CommandTextBox);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.SendCommandButton);
+			this.groupBox2.Location = new System.Drawing.Point(297, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(237, 162);
+			this.groupBox2.TabIndex = 6;
+			this.groupBox2.TabStop = false;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(6, 116);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(52, 17);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Ответ:";
+			// 
+			// AnswerTextBox
+			// 
+			this.AnswerTextBox.Enabled = false;
+			this.AnswerTextBox.Location = new System.Drawing.Point(64, 116);
+			this.AnswerTextBox.Name = "AnswerTextBox";
+			this.AnswerTextBox.Size = new System.Drawing.Size(158, 22);
+			this.AnswerTextBox.TabIndex = 5;
+			// 
+			// CommandTextBox
+			// 
+			this.CommandTextBox.Location = new System.Drawing.Point(41, 43);
+			this.CommandTextBox.Name = "CommandTextBox";
+			this.CommandTextBox.Size = new System.Drawing.Size(145, 22);
+			this.CommandTextBox.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(50, 18);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(131, 17);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Командная строка";
+			// 
+			// SendCommandButton
+			// 
+			this.SendCommandButton.Location = new System.Drawing.Point(66, 71);
+			this.SendCommandButton.Name = "SendCommandButton";
+			this.SendCommandButton.Size = new System.Drawing.Size(91, 24);
+			this.SendCommandButton.TabIndex = 1;
+			this.SendCommandButton.Text = "Отправить";
+			this.SendCommandButton.UseVisualStyleBackColor = true;
+			this.SendCommandButton.Click += new System.EventHandler(this.SendCommandButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(559, 273);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.labelTemp);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comPortscomboBox;
+		private System.Windows.Forms.Button connectButton;
+		private System.Windows.Forms.Label labelTemp;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button UpdatePortsButton;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox AnswerTextBox;
+		private System.Windows.Forms.TextBox CommandTextBox;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button SendCommandButton;
+	}
+}
+
---
+++ WindowsSerial/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+/// <summary>
+/// Автор программы: Кобелева В.О.
+/// Версия: 1.0
+/// Дата версии: 07.04.2022
+/// 
+/// Дата передачи: 07.04.2022
+/// Соавтор: Бовкун Е.А.
+/// Версия 1.1
+/// Дата версии: 
+/// /// </summary>
+
+namespace WindowsSerial
+{
+	public partial class Form1 : Form
+	{
+		private SerialPort serial;
+		private bool boundTest = false;
+		private bool databitsTest = false;
+		private bool isBoundrateCorrect = false;
+		private bool isDatabitsCorrect = false;
+
+		public Form1()
+		{
+			InitializeComponent();
+			serial = new SerialPort(); //экзмепляр класса последовательного порта
+			serial.DataReceived += OnReceived; //добавляем обработчик событий при получении данных
+			UpdateComportNames();
+		}
+
+		private void connectButton_Click(object sender, EventArgs e)
+		{
+			if (serial.IsOpen) //если порт открыт
+			{
+				serial.Close();
+				Console.WriteLine("Закрыто");
+				connectButton.Text = "Connect";
+				//SendCommandButton.Enabled = false; //делаем кнопку отправки команды доступной
+			}
+			else
+			{
+				connectButton.Text = "Disconnect";
+				//устанавливаем параметры передачи данных
+				serial.BaudRate = 9600;
+				serial.DtrEnable = true; //терминал готов к передаче
+				serial.DataBits = 8;
+				serial.PortName = (string)comPortscomboBox.SelectedItem;
+				boundTest = false; //тест на BoundRate не закончен
+				databitsTest = false; //тест на DataBits не закончен
+				isBoundrateCorrect = false; //статус теста - завален
+				isDatabitsCorrect = false;
+				serial.Open();
+				serial.DiscardInBuffer(); //очистить буфер входных данных
+				Console.WriteLine("Открыто");
+			}
+		}
+
+		private void OnReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+			if (!boundTest) //если не завершен boundTest
+			{
+				int recivedByte = serial.ReadByte();
+				boundTest = true;
+				if (recivedByte == 1)
+				{
+					isBoundrateCorrect = true;
+					Console.WriteLine("Boundrate good");
+				}
+				else
+				{
+					Console.WriteLine("Boundrate bad");
+				}
+			}
+			else if (!databitsTest) //если не завершен databitsTest
+			{
+				int recivedByte = serial.ReadByte();
+				databitsTest = true;
+				if (recivedByte == 255)
+				{
+					isDatabitsCorrect = true;
+					Console.WriteLine("Databits good");
+					//SendCommandButton.Enabled = true; //делаем кнопку отправки команды доступной
+				}
+				else
+				{
+					Console.WriteLine("Databits bad");
+				}
+			}
+			else if (isBoundrateCorrect && isDatabitsCorrect) //если успешно пройдены все тесты, принимаем данные
+			{
+				String recivedMessage = serial.ReadLine();
+				recivedMessage = recivedMessage.Replace("\r", "").Replace("\n", "");
+				if (recivedMessage.StartsWith("temp:"))
+				{
+					ToLabelTemp(recivedMessage.Replace("temp:",""));
+				}
+				else
+				{
+					String[] partsOfMessage = recivedMessage.Split(':');
+					try
+					{
+						ToAnswerTextBox(partsOfMessage[1]);
+					}
+					catch (Exception)
+					{
+						ToAnswerTextBox(recivedMessage);
+					}
+				}
+			}
+		}
+		private void ToLabelTemp(string text) //функция изменения содержимого Label
+		{
+			if (InvokeRequired)
+				Invoke((Action<string>)ToLabelTemp, text);
+			else
+				labelTemp.Text = text;
+		}
+
+		private void ToAnswerTextBox(string text) //функция изменения содержимого AnswerTextBox
+		{
+			if (InvokeRequired)
+				Invoke((Action<string>)ToAnswerTextBox, text);
+			else
+				AnswerTextBox.Text = text;
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			serial.Close();
+		}
+
+		private int UpdateComportNames()
+		{
+			comPortscomboBox.Items.Clear();
+			// Получаем список COM портов доступных в системе
+			string[] portnames = SerialPort.GetPortNames();
+			// Проверяем есть ли доступные
+			if (portnames.Length == 0)
+			{
+				MessageBox.Show("COM PORT not found");
+			}
+
+			foreach (string portName in portnames)
+			{
+				//добавляем доступные COM порты в список           
+				comPortscomboBox.Items.Add(portName);
+				Console.WriteLine(portnames.Length);
+			}
+			if (portnames.Length != 0)
+			{
+				comPortscomboBox.SelectedItem = portnames[0];
+			}
+			return (portnames.Length);
+		}
+
+		private void UpdatePortsButton_Click(object sender, EventArgs e)
+		{
+			UpdateComportNames();
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void SendCommandButton_Click(object sender, EventArgs e)
+		{
+			string InputCommand = CommandTextBox.Text;
+			if (isBoundrateCorrect && isDatabitsCorrect)
+			{
+				WriteData(InputCommand);
+			}
+		}
+		private void WriteData (string data)
+		{
+			serial.WriteLine(data);
+		}
+	}
+}
